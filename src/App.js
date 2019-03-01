022e5b2e8eb5c4aedc3d31d6e550654ac3fadc6d
j@jonathangee.com <j@jonathangee.com>
2019-02-28 22:44:34 -0800

    created redux store and added Provider to entry index class. Removed spaces from App class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+			</div>
+		);
+	}
 }
 
 export default App;
