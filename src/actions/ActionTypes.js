429b3610b536962c0abf054d1c1d10ab3ba82769
Jonathan Gee <j@jonathangee.com>
2020-10-15 19:21:31 -0700

    feat: removed old project

--- src/actions/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-// start to gather apod
-export const FETCH_APOD = 'fetch astronomy picture of the day';
-// 
-export const REQUEST_APOD = 'request astronomy picture of the day';
-export const RECIEVE_APOD = 'recieve astronomy picture of the day';
-
-export const SET_DATE = 'set date';
